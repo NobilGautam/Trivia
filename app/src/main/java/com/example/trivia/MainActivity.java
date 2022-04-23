7b972f2881f147cae5c871c71df20aa6ed6f8f82
Nobil Gautam <nobilgautam007@gmail.com>
2022-04-23 13:50:08 +0530

    UI Created

--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -1,23 +1,26 @@
 package com.example.trivia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.util.Log;
 
 import com.example.trivia.data.QuestionListAsyncResponse;
 import com.example.trivia.data.Repository;
+import com.example.trivia.databinding.ActivityMainBinding;
 import com.example.trivia.model.Question;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Repository repository = (Repository) new Repository().getQuestions(questions ->
                 Log.d("DATA", questions.toString()));
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
