1e2ed88678a581f761d76ee2cf00c5005d5e771c
Nobil Gautam <nobilgautam007@gmail.com>
2022-04-25 16:54:32 +0530

    Final Commit: Ready to publish

--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.trivia;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,15 +25,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String CURRENT_SCORE = "CurrentScore";
     private ActivityMainBinding binding;
+    List<Question> questionBank;
     private int currentQuestionIndex = 0;
+    int currentScore = 0;
+    int highScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        List<Question> questionBank = new Repository().getQuestions(new QuestionListAsyncResponse() {
+        questionBank = new Repository().getQuestions(new QuestionListAsyncResponse() {
             @Override
             public void processFinished(ArrayList<Question> questions) {
                 //Initializing the UI
@@ -37,13 +46,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        binding.currentScoreTextView.setText("Current Score: " + currentScore);
+        SharedPreferences sharedPreferences = getSharedPreferences(CURRENT_SCORE, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        SharedPreferences getSharedData = getSharedPreferences(CURRENT_SCORE, MODE_PRIVATE);
+        highScore = getSharedData.getInt("current", 0);
+        binding.highScoreTextView.setText("High Score: " + String.valueOf(highScore));
+
         //Checking answer
         binding.trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(questionBank.get(currentQuestionIndex).getAnswer() == true) {
                     Toast.makeText(MainActivity.this, "That's correct!", Toast.LENGTH_SHORT).show();
-                } else Toast.makeText(MainActivity.this, "That's wrong!", Toast.LENGTH_SHORT).show();
+                    currentScore++;
+                    fadeAnimation();
+                    if (currentScore > highScore) {
+                        highScore = currentScore;
+                        editor.putInt("current", highScore);
+                        editor.apply();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "That's wrong!", Toast.LENGTH_SHORT).show();
+                    shakeAnimation();
+                    if(currentScore != 0) {
+                        currentScore--;
+                    }
+                }
+                binding.currentScoreTextView.setText("Current Score: " + currentScore);
+                updateQues();
             }
         });
         binding.falseButton.setOnClickListener(new View.OnClickListener() {
@@ -51,7 +83,22 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if(questionBank.get(currentQuestionIndex).getAnswer() == false) {
                     Toast.makeText(MainActivity.this, "That's correct!", Toast.LENGTH_SHORT).show();
-                } else Toast.makeText(MainActivity.this, "That's wrong!", Toast.LENGTH_SHORT).show();
+                    currentScore++;
+                    fadeAnimation();
+                    if (currentScore > highScore) {
+                        highScore = currentScore;
+                        editor.putInt("current", highScore);
+                        editor.apply();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "That's wrong!", Toast.LENGTH_SHORT).show();
+                    shakeAnimation();
+                    if(currentScore != 0) {
+                        currentScore--;
+                    }
+                }
+                binding.currentScoreTextView.setText("Current Score: " + currentScore);
+                updateQues();
             }
         });
 
@@ -59,10 +106,60 @@ public class MainActivity extends AppCompatActivity {
         binding.nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                currentQuestionIndex = (currentQuestionIndex + 1) % questionBank.size();
-                binding.questionIndexTextView.setText("Question: " + (currentQuestionIndex+1) + "/" + questionBank.size());
-                binding.questionTextView.setText(questionBank.get(currentQuestionIndex).getQuestion());
+                updateQues();
             }
         });
+    }
+
+    private void updateQues() {
+        currentQuestionIndex = (currentQuestionIndex + 1) % questionBank.size();
+        binding.questionIndexTextView.setText("Question: " + (currentQuestionIndex+1) + "/" + questionBank.size());
+        binding.questionTextView.setText(questionBank.get(currentQuestionIndex).getQuestion());
+    }
+
+    private void shakeAnimation() {
+        Animation shake = AnimationUtils.loadAnimation(MainActivity.this, R.anim.rotate);
+        shake.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                binding.questionTextView.setTextColor(Color.RED);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                binding.questionTextView.setTextColor(getResources().getColor(R.color.textColor));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        binding.cardView.setAnimation(shake);
+    }
+
+    private void fadeAnimation() {
+        AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f, 0.0f);
+        alphaAnimation.setDuration(150);
+        alphaAnimation.setRepeatMode(Animation.REVERSE);
+        alphaAnimation.setRepeatCount(1);
+
+        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                binding.questionTextView.setTextColor(Color.GREEN);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                binding.questionTextView.setTextColor(getResources().getColor(R.color.textColor));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        binding.cardView.setAnimation(alphaAnimation);
     }
 }
